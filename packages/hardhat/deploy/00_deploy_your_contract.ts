94742decf264cb7cad0c5ffd1d598c520a8a8138
MM <xxxxxx@users.noreply.github.com>
2024-10-06 08:53:11 +0200

    merge

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -22,10 +22,22 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("NunyaBusiness", {
+
+  const Gateway = await deploy("SecretContract", {
     from: deployer,
     // Contract constructor arguments
     args: [deployer],
+    log: true,
+    gasLimit: 3000000, 
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  await deploy("NunyaBusiness", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [deployer, Gateway.address],
     log: true,
     gasLimit: 3000000, 
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
@@ -42,4 +54,4 @@ export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["NunyaBusiness"];
+deployYourContract.tags = ["NunyaBusiness", "SecretContract"];
