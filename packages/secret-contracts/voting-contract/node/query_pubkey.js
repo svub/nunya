72bdaa3caaecfb069b78d85e6b4e4b429bca68f8
Luke Schoen <ltfschoen@gmail.com>
2024-10-06 08:08:03 +0200

    attempt to build secret contract

--- packages/secret-contracts/voting-contract/node/query_pubkey.js
+++ packages/secret-contracts/voting-contract/node/query_pubkey.js
@@ -4,14 +4,14 @@ dotenv.config();
 
 let query = async () => {
   const secretjs = new SecretNetworkClient({
-    url: "https://lcd.testnet.secretsaturn.net",
+    url: "https://api.pulsar3.scrttestnet.com",
     chainId: "pulsar-3",
   });
 
   const query_tx = await secretjs.query.compute.queryContract({
     contract_address: process.env.SECRET_ADDRESS,
     code_hash: process.env.CODE_HASH,
-    query: { retrieve_bids: { key: 1 } },
+    query: { retrieve_pubkey_query: { key: 1 } },
   });
   console.log(query_tx);
 };
