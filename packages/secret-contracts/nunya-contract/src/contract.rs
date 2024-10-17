97063821e5e4d3246df78611141dd16bfbd95f3c
Luke Schoen <ltfschoen@gmail.com>
2024-10-17 14:39:24 +0200

    fix withdraw reduce balance and add initial tests

--- packages/secret-contracts/nunya-contract/src/contract.rs
+++ packages/secret-contracts/nunya-contract/src/contract.rs
@@ -607,12 +607,36 @@ fn create_withdraw_to(
     let mut balance_all_payment_refs: Uint128 = 0u128.into();
     for element in value_payment_reference_to_balances.into_iter() {
         if element.balance.denom == denomination {
-            balance_all_payment_refs += element.balance.amount
+            balance_all_payment_refs.checked_add(element.balance.amount).ok_or(StdError::generic_err(err.to_string()))?;
         }
     }
 
     if amount > balance_all_payment_refs {
         return Err(StdError::generic_err("Withdrawal amount must be less than or equal to total balance of all payment references"));
+    }
+
+    // Subtract the withdrawal amount sequentially from the balance associated with each payment reference stored in
+    // VIEWING_KEY_TO_PAYMENT_REF_TO_BALANCES_MAP
+
+    let mut remaining_amount = amount.clone();
+    let mut new_balance_all_payment_refs: Uint128 = 0u128.into();
+
+    for element in value_payment_reference_to_balances.into_iter() {
+        let existing_element_balance = element.balance.amount.clone();
+        if remaining_amount >= existing_element_balance {
+            // remove payment ref balance to later update in storage
+            value_payment_reference_to_balances[element].balance.amount = 0u128;
+
+            // subtract from remaining_amount
+            // TODO: use `Error::<T>::Overflow`
+            remaining_amount = remaining_amount.checked_sub(&existing_element_balance.clone()).ok_or(StdError::generic_err(err.to_string()))?;
+        } else {
+            // new payment ref balance
+            value_payment_reference_to_balances[element].balance.amount = existing_element_balance
+                .checked_sub(&remaining_amount.clone()).ok_or(StdError::generic_err(err.to_string()))?;
+
+            remaining_amount = 0u128;
+        }
     }
 
     let response_status_code: ResponseStatusCode = 0u16;
@@ -647,7 +671,7 @@ fn create_withdraw_to(
 
     Ok(Response::new()
         .add_message(callback_msg)
-        .add_attribute("status", "DO_THE_WITHDRAWAL"))
+        .add_attribute("status", "create_withdraw_to"))
 }
 
 // reference: https://github.com/writersblockchain/secretpath-voting/commit/b0b5d8ac7b7d691c7d7ebf0bc52e5aedb8da7e86#diff-a982e501ba4bd05192a8c497bd5093517ea5606f9e341b9b7d09b233068da829R232
@@ -722,5 +746,94 @@ mod tests {
         let res = query(deps.as_ref(), mock_env(), QueryMsg::RetrievePubkey {}).unwrap();
         let res: ResponseRetrievePubkeyMsg = from_binary(&res).unwrap();
         assert!(res._key);
+    }
+
+    #[test]
+    fn withdraw_reduces_withdrawal_amount_from_payment_ref_storage_balances() {
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info("sender", &[]);
+        let init_msg = InstantiateMsg {
+            gateway_address: Addr::unchecked("fake address".to_string()),
+            gateway_hash: "fake code hash".to_string(),
+            gateway_key: Binary(b"fake key".to_vec()),
+        };
+        instantiate(deps.as_mut(), env.clone(), info.clone(), init_msg).unwrap();
+
+        // retrieve_pubkey
+        let message = PrivContractHandleMsg {
+            input_values: "{}".to_string(),
+            handle: "retrieve_pubkey".to_string(),
+            user_address: Addr::unchecked("0x1".to_string()),
+            task_id: 1,
+            input_hash: to_binary(&"".to_string()).unwrap(),
+            signature: to_binary(&"".to_string()).unwrap(),
+        };
+        let handle_msg = ExecuteMsg::Input { message };
+        let handle_response =
+            execute(deps.as_mut(), env.clone(), info.clone(), handle_msg).unwrap();
+        let result = &handle_response.attributes["status"].value;
+        assert_eq!(result, "retrieve_pubkey");
+
+        // create_new_auth_out
+        let message = PrivContractHandleMsg {
+            input_values: "{\"secret_user\":\"abc\"}".to_string(),
+            handle: "create_new_auth_out".to_string(),
+            user_address: Addr::unchecked("0x1".to_string()),
+            task_id: 2,
+            input_hash: to_binary(&"".to_string()).unwrap(),
+            signature: to_binary(&"".to_string()).unwrap(),
+        };
+        let handle_msg = ExecuteMsg::Input { message };
+        let handle_response =
+            execute(deps.as_mut(), env.clone(), info.clone(), handle_msg).unwrap();
+        let result = &handle_response.attributes["status"].value;
+        assert_eq!(result, "create_new_auth_out");
+        // TODO: verify it returns _request_id and _code values
+
+        // create_payment_reference
+        let message = PrivContractHandleMsg {
+            input_values: "{\"secret_user\":\"abc\",\"payment_ref\":\"def\"}".to_string(),
+            handle: "create_payment_reference".to_string(),
+            user_address: Addr::unchecked("0x1".to_string()),
+            task_id: 3,
+            input_hash: to_binary(&"".to_string()).unwrap(),
+            signature: to_binary(&"".to_string()).unwrap(),
+        };
+        let handle_msg = ExecuteMsg::Input { message };
+        let handle_response =
+            execute(deps.as_mut(), env.clone(), info.clone(), handle_msg).unwrap();
+        let result = &handle_response.attributes["status"].value;
+        assert_eq!(result, "create_payment_reference");
+
+        // create_pay
+        let message = PrivContractHandleMsg {
+            input_values: "{\"secret_user\":\"abc\",\"payment_ref\":\"def\",\"amount\":100,\"denomination\":\"ETH\",\"user_pubkey\":\"0x3\"}".to_string(),
+            handle: "create_pay".to_string(),
+            user_address: Addr::unchecked("0x1".to_string()),
+            task_id: 4,
+            input_hash: to_binary(&"".to_string()).unwrap(),
+            signature: to_binary(&"".to_string()).unwrap(),
+        };
+        let handle_msg = ExecuteMsg::Input { message };
+        let handle_response =
+            execute(deps.as_mut(), env.clone(), info.clone(), handle_msg).unwrap();
+        let result = &handle_response.attributes["status"].value;
+        assert_eq!(result, "create_pay");
+
+        // create_withdraw_to
+        let message = PrivContractHandleMsg {
+            input_values: "{\"secret_user\":\"abc\",\"amount\":100,\"denomination\":\"ETH\",\"withdrawal_address\":\"0x2\"}".to_string(),
+            handle: "create_withdraw_to".to_string(),
+            user_address: Addr::unchecked("0x1".to_string()),
+            task_id: 5,
+            input_hash: to_binary(&"".to_string()).unwrap(),
+            signature: to_binary(&"".to_string()).unwrap(),
+        };
+        let handle_msg = ExecuteMsg::Input { message };
+        let handle_response =
+            execute(deps.as_mut(), env.clone(), info.clone(), handle_msg).unwrap();
+        let result = &handle_response.attributes["status"].value;
+        assert_eq!(result, "create_withdraw_to");
     }
 }
