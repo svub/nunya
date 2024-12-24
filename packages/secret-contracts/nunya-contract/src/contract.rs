730a13c401369caec718cbea80fb774997f4b179
Luke Schoen <ltfschoen@gmail.com>
2024-12-25 09:35:46 +1100

    tweak Secret Nunya contract debugging

--- packages/secret-contracts/nunya-contract/src/contract.rs
+++ packages/secret-contracts/nunya-contract/src/contract.rs
@@ -98,6 +98,7 @@ fn try_handle(
     deps.api.debug(format!("try_handle").as_str());
     deps.api.debug(format!("msg: {:#?}", msg.clone()).as_str());
     deps.api.debug(format!("info: {:#?}", info.clone()).as_str());
+    deps.api.debug(format!("config: {:#?}", config.clone()).as_str());
 
     // Security
     //
@@ -119,11 +120,20 @@ fn try_handle(
     // }
 
     // FIXME: Generic error: Invalid public key format
+    //
+    // Note: The serialized public key should be &[u8] and in one of the two supported formats:
+    // 1. Uncompressed: 65 bytes starting with 0x04
+    // 2. Compressed: 33 bytes starting with 0x02 or 0x03
+    deps.api.debug(format!("config.gateway_key.len(): {:#?}", config.gateway_key.len()).as_str());
+    deps.api.debug(format!("config.gateway_key.as_bytes().len(): {:#?}", config.gateway_key.as_bytes().len()).as_str());
+
+    let gateway_key_bytes: &Bytes = &config.gateway_key.as_bytes();
+
     deps.api
         .secp256k1_verify(
             msg.input_hash.as_slice(),
             msg.signature.as_slice(),
-            config.gateway_key.as_bytes(),
+            gateway_key_bytes.to_vec(), // &[u8]
         )
         .map_err(|err| StdError::generic_err(err.to_string()))?;
 
